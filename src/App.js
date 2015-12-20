30d1d7e55106b6fe6a649e86a0c04f2c80a588f0
Interns <interns@Revelations-MacBook-Pro.local>
2015-12-19 18:03:17 -0800

    Refactor App component extracting FilterLink component into Footer, Link, and FilterLink components

--- src/App.js
+++ src/App.js
@@ -1,56 +1,40 @@
-import React, { Component } from 'react';
-import {FilterLink} from './FilterLink';
+import React from 'react';
 import {TodoList} from './TodoList';
 import {AddTodo} from './AddTodo';
+import {Footer} from './Footer';
 
 let nextTodoId = 0;
 
-export default class App extends Component {
-  render() {
-    const {
-      todos,
-      visibilityFilter,
-      store
-    } = this.props;
-    const visibleTodos = this.props.getVisibleTodos(
-      todos,
-      visibilityFilter
-    );
-    return (
-      <div>
-        <AddTodo
-          onAddClick={text =>
-            store.dispatch({
-              type: 'ADD_TODO',
-              id: nextTodoId++,
-              text
-            })
-          }
-        />
-        <TodoList
-           todos={visibleTodos}
-          onTodoClick={id =>
-            store.dispatch({
-              type: 'TOGGLE_TODO',
-              id
-            })
-          } />
-        <p>
-          Show:
-          {' '}
-          <FilterLink filter='SHOW_ALL' store={store} currentFilter={visibilityFilter}>
-            All
-          </FilterLink>
-          {' '}
-          <FilterLink filter='SHOW_ACTIVE' store={store} currentFilter={visibilityFilter}>
-            Active
-          </FilterLink>
-          {' '}
-          <FilterLink filter='SHOW_COMPLETED' store={store} currentFilter={visibilityFilter}>
-            Completed
-          </FilterLink>
-        </p>
-      </div>
-    );
-  }
+export const App = ({
+  todos,
+  visibilityFilter,
+  store,
+  getVisibleTodos
+}) => {
+  return (
+    <div>
+      <AddTodo
+        onAddClick={text =>
+          store.dispatch({
+            type: 'ADD_TODO',
+            id: nextTodoId++,
+            text
+          })
+        }
+      />
+      <TodoList
+         todos={
+           getVisibleTodos(
+             todos,
+             visibilityFilter
+           )}
+        onTodoClick={id =>
+          store.dispatch({
+            type: 'TOGGLE_TODO',
+            id
+          })
+        } />
+      <Footer store={store} />
+    </div>
+  );
 }
--- src/FilterLink.js
+++ src/FilterLink.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Link} from './Link';
 
-export const FilterLink = ({
-  filter,
-  children,
-  store,
-  currentFilter
-}) => {
-  if ( filter === currentFilter) {
-    return <span>{children}</span>
+export default class FilterLink extends Component {
+  componentDidMount() {
+    const { store } = this.props;
+    this.unsubscribe = store.subscribe(() =>
+      this.forceUpdate()
+    )
   }
-  return (
-    <a href='#' onClick={e => {
-      e.preventDefault();
-      store.dispatch({
-        type: 'SET_VISIBILITY_FILTER',
-        filter
-      });
-    }}>
-      {children}
-    </a>
-  );
-};
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  render() {
+    const {store, filter, children} = this.props;
+    const state = store.getState();
+    return (
+      <Link
+        active={
+          filter === state.visibilityFilter
+        }
+        onClick={() =>
+          store.dispatch({
+            type: 'SET_VISIBILITY_FILTER',
+            filter: filter
+          })
+        }
+      >
+        {children}
+      </Link>
+    );
+  }
+}
